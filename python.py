fb1cc8fb47795a577242d4dda1e127de3191816e
linux-best <amirmahdifhp@gmail.com>
2025-03-07 11:30:29 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,17 +1,17 @@
-path="/home/javande/file10"
+path="/var/lib/jenkins/workspace/my_python_job/file10"
 
 print(path)
-#def stuff(n):
-#    count = 2
-#    symbol = "*"
-#    Coefficient = 19
-#    index = 1
-#    while index <= n:
-#        space = Coefficient * " "
-#        print(space+(symbol*count))
-#        Coefficient-=1
-#        count+=2
-#        index+=1
+def stuff(n):
+    count = 2
+    symbol = "*"
+    Coefficient = 19
+    index = 1
+    while index <= n:
+        space = Coefficient * " "
+        print(space+(symbol*count))
+        Coefficient-=1
+        count+=2
+        index+=1
 
-#with open(path , "w") as x :
-#    x.write(stuff(20))
+with open(path , "w") as x :
+    x.write(stuff(20))
