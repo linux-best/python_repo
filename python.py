4b8c1e82103cf9837d583b2733064eaf16e6deb9
linux-best <amirmahdifhp@gmail.com>
2025-03-07 11:06:59 -0500

    Add ,Update python_script ,repo

---
+++ python.py
@@ -0,0 +1,12 @@
+def stuff(n):
+    count = 2
+    symbol = "*"
+    Coefficient = 19
+    index = 1
+    while index <= n:
+        space = Coefficient * " "
+        print(space+(symbol*count))
+        Coefficient-=1
+        count+=2
+        index+=1
+stuff(20)
