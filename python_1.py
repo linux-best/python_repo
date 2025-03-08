8b36c1b8bb5269dbea75981b86afcb03ba135082
linux-best <amirmahdifhp@gmail.com>
2025-03-08 15:51:06 +0330

    Update python_1.py

--- python_1.py
+++ python_1.py
@@ -1,5 +1,7 @@
 from loguru import logger
 
+path="/var/lib/jenkins/workspace/my_python_Job/file10"
+
 logger.add(path ,
            format="<yellow>{time: MMMM D, YYYY - HH:mm:ss}</yellow> -- <green>{level}</green> -- <level>{message}</level> {extra}",
            level="DEBUG"
